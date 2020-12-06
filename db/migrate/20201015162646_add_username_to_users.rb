4ecc236b03bf06218696f9ae18f966f8b83b197f
Kemal MUTLU <mutlukemal1717@gmail.com>
2020-12-07 02:54:39 +0300

    Add category seeds and category model

--- db/migrate/20201015162646_add_username_to_users.rb
+++ db/migrate/20201015162646_add_username_to_users.rb
@@ -1,4 +1,4 @@
-class AddUsernameToUsers < ActiveRecord::Migration[6.0]
+class AddUsernameToUsers < ActiveRecord::add_index :users, :username, unique: trueMigration[6.0]
   def change
     add_column :users, :username, :string
     add_index :users, :username, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_145141) do
+ActiveRecord::Schema.define(version: 2020_12_06_232406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2020_11_07_145141) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.boolean "tradable", default: false, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
