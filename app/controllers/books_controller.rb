0085e436a6d6a0edd4e283ba1ab8e835d4070429
Kemal MUTLU <mutlukemal1717@gmail.com>
2020-12-07 15:38:36 +0300

    Update the book controller to add a category.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,28 +12,40 @@ class BooksController < ApplicationController
 
   def new   
     @book = Book.new
+    category_collection
   end
 
   def edit
     @book = Book.find(params[:id])
+    category_collection
   end
 
   def create
     @book = current_user.books.build(book_params)
-    
+    @category = Category.where(id: params[:book]["categories"])
+    @book.categories << @category
+
     if @book.save
       redirect_to @book
     else
+      category_collection
       render 'new'
     end
   end
 
   def update
     @book = Book.find(params[:id])
+    @category = Category.where(id: params[:book]["categories"])
+
+    # First remove all category
+    @book.categories.destroy_all
+
+    @book.categories << @category
 
     if @book.update(book_params)
       redirect_to @book
     else
+      category_collection
       render 'edit'
     end
   end
@@ -48,6 +60,10 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :author, :pagecount, :status, :user_id, :tradable)
+  end
+
+  def category_collection
+    @categories = Category.all.collect { |category| [category.name, category.id] }
   end
 
   def require_permission
